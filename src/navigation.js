0e53b06d25715868532f35285cfe4ba1e8ce7f59
Christoph von Gellhorn <gellborn@gmail.com>
2024-07-09 21:38:21 +1000

    chore: update content, header and footer

--- src/navigation.js
+++ src/navigation.js
@@ -3,26 +3,31 @@ import { getPermalink, getBlogPermalink } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Products',
+      text: 'Shopify Apps',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'Free Shipping Bar',
+          href: '#'
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Checkout Notes',
+          href: '#'
         },
       ],
     },
+    // {
+    //   text: 'Products',
+    //   links: [
+    //     {
+    //       text: 'Shopify Apps',
+    //       href: getPermalink('/homes/saas'),
+    //     },
+    //     {
+    //       text: 'Coming Soon',
+    //       href: getPermalink('/homes/saas'),
+    //     },
+    //   ],
+    // },
     // {
     //   text: 'Pages',
     //   links: [
@@ -58,7 +63,7 @@ export const headerData = {
     // },
     {
       text: 'About',
-      href: getPermalink('/landing/sales'),
+      href: getPermalink('/about'),
       // links: [
       //   {
       //     text: 'Lead Generation',
@@ -121,12 +126,8 @@ export const footerData = {
     {
       title: 'Product',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
         { text: 'Pricing', href: '#' },
+        { text: 'Features', href: '#' },
         { text: 'Resources', href: '#' },
       ],
     },
@@ -135,9 +136,6 @@ export const footerData = {
       links: [
         { text: 'Developer API', href: '#' },
         { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
       ],
     },
     {
@@ -146,8 +144,6 @@ export const footerData = {
         { text: 'Docs', href: '#' },
         { text: 'Community Forum', href: '#' },
         { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
       ],
     },
     {
@@ -155,11 +151,9 @@ export const footerData = {
       links: [
         { text: 'About', href: '#' },
         { text: 'Blog', href: '#' },
+        { text: 'Team', href: '#' },
         { text: 'Careers', href: '#' },
         { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
       ],
     },
   ],
@@ -168,14 +162,9 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/sturmfreico' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/sturmfreico/' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/sturmfreico' },
-    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
-  // footNote: `
-  //   <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-  //   Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  // `,
+  footNote: `© 2024 Sturmfrei Pty Ltd · All rights reserved`,
 };
