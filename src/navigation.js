b0ce55ad4f367f894b4970f4e2eedddfa4e17bd8
Christoph von Gellhorn <gellborn@gmail.com>
2024-07-11 22:41:42 +1000

    chore: update content

--- src/navigation.js
+++ src/navigation.js
@@ -10,153 +10,63 @@ export const headerData = {
           href: '#'
         },
         {
-          text: 'Checkout Notes',
+          text: 'More Coming Soon',
           href: '#'
         },
       ],
     },
-    // {
-    //   text: 'Products',
-    //   links: [
-    //     {
-    //       text: 'Shopify Apps',
-    //       href: getPermalink('/homes/saas'),
-    //     },
-    //     {
-    //       text: 'Coming Soon',
-    //       href: getPermalink('/homes/saas'),
-    //     },
-    //   ],
-    // },
-    // {
-    //   text: 'Pages',
-    //   links: [
-    //     {
-    //       text: 'Features (Anchor Link)',
-    //       href: getPermalink('/#features'),
-    //     },
-    //     {
-    //       text: 'Services',
-    //       href: getPermalink('/services'),
-    //     },
-    //     {
-    //       text: 'Pricing',
-    //       href: getPermalink('/pricing'),
-    //     },
-    //     {
-    //       text: 'About us',
-    //       href: getPermalink('/about'),
-    //     },
-    //     {
-    //       text: 'Contact',
-    //       href: getPermalink('/contact'),
-    //     },
-    //     {
-    //       text: 'Terms',
-    //       href: getPermalink('/terms'),
-    //     },
-    //     {
-    //       text: 'Privacy policy',
-    //       href: getPermalink('/privacy'),
-    //     },
-    //   ],
-    // },
+    {
+      text: 'FAQS',
+      href: getPermalink('/faq'),
+    },
     {
       text: 'About',
       href: getPermalink('/about'),
-      // links: [
-      //   {
-      //     text: 'Lead Generation',
-      //     href: getPermalink('/landing/lead-generation'),
-      //   },
-      //   {
-      //     text: 'Long-form Sales',
-      //     href: getPermalink('/landing/sales'),
-      //   },
-      //   {
-      //     text: 'Click-Through',
-      //     href: getPermalink('/landing/click-through'),
-      //   },
-      //   {
-      //     text: 'Product Details (or Services)',
-      //     href: getPermalink('/landing/product'),
-      //   },
-      //   {
-      //     text: 'Coming Soon or Pre-Launch',
-      //     href: getPermalink('/landing/pre-launch'),
-      //   },
-      //   {
-      //     text: 'Subscription',
-      //     href: getPermalink('/landing/subscription'),
-      //   },
-      // ],
     },
     {
       text: 'Blog',
       href: getBlogPermalink(),
-      // links: [
-      //   {
-      //     text: 'Blog List',
-      //     href: getBlogPermalink(),
-      //   },
-      //   {
-      //     text: 'Article',
-      //     href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-      //   },
-      //   {
-      //     text: 'Article (with MDX)',
-      //     href: getPermalink('markdown-elements-demo-post', 'post'),
-      //   },
-      //   {
-      //     text: 'Category Page',
-      //     href: getPermalink('tutorials', 'category'),
-      //   },
-      //   {
-      //     text: 'Tag Page',
-      //     href: getPermalink('astro', 'tag'),
-      //   },
-      // ],
     },
   ],
   // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Pricing', href: '#' },
-        { text: 'Features', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-      ],
-    },
-  ],
+  // links: [
+  //   {
+  //     title: 'Product',
+  //     links: [
+  //       { text: 'Pricing', href: '#' },
+  //       { text: 'Features', href: '#' },
+  //       { text: 'Resources', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Platform',
+  //     links: [
+  //       { text: 'Developer API', href: '#' },
+  //       { text: 'Partners', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Support',
+  //     links: [
+  //       { text: 'Docs', href: '#' },
+  //       { text: 'Community Forum', href: '#' },
+  //       { text: 'Professional Services', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Company',
+  //     links: [
+  //       { text: 'About', href: '#' },
+  //       { text: 'Blog', href: '#' },
+  //       { text: 'Team', href: '#' },
+  //       { text: 'Careers', href: '#' },
+  //       { text: 'Press', href: '#' },
+  //     ],
+  //   },
+  // ],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
