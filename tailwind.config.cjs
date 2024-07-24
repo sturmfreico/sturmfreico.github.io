17da517ec6067b6b33d777afe15375d20b506727
Christoph von Gellhorn <gellborn@gmail.com>
2024-07-24 21:23:51 +1000

    chore: update about us

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,8 @@ import typographyPlugin from '@tailwindcss/typography';
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  plugins: [typographyPlugin],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -22,6 +24,4 @@ module.exports = {
       },
     },
   },
-  plugins: [typographyPlugin],
-  darkMode: 'class',
 };
